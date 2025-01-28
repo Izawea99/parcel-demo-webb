3c0a399f86c33d0a2685fffbff3f58b693989589
Izabel Weatherly <izabelweatherly99@gmail.com>
2025-01-28 16:58:35 +0100

    Tillägg av en hamburger-meny för mindre skärmar med tillhörande javascript, css och html. Samt tillägg i package.json-filen för kompabilitet med webbläsare som används av mer än 25% globalt samt inkludera senaste två versionerna och utesluta webbläsare som inte blivit uppdaterade på länge.

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+const burgerMenu = document.getElementById('burger-menu');
+const headMenu = document.getElementById('headmenu');
+
+
+burgerMenu.addEventListener('click', () => {
+    headMenu.classList.toggle('show');
+});
+
+
+
